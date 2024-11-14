7d091778be6d68600791d2001f8b07ecf2b961f7
nanyanen87 <nan.hanaoka@gmail.com>
2024-11-15 04:27:04 +0900

    エラーハンドリングおかしかった

--- commands/tensordock/stop.js
+++ commands/tensordock/stop.js
@@ -9,33 +9,46 @@ module.exports = {
         .setDescription('serverをstopします。'),
     async execute(interaction) {
         const tensordock = new TensorDock();
-        const server_id = interaction.options.getString('server_id');
-        // const server_info = await tensordock.detail(server_id);
-        // const stop_param = {
-        //     server: server_id,
-        //     gpu_model: server_info.specs.gpu.type,
-        //     gpu_count: 0,
-        //     ram: 1,
-        //     vcpus: 2,
-        //     storage: server_info.specs.storage,
-        // }
-        // // modify
-        // const modify_res = await tensordock.modify(stop_param);
-        // if (modify_res === false){
-        //     await interaction.reply(`modifyに失敗しました。`);
-        //     return;
-        // }
-        // stop
-        const res = await tensordock.stop(server_id);
-        if (res.success === false){
+        const serverId = interaction.options.getString('server_id');
+        const serverInfo = await tensordock.detail(serverId);
+        const stopParam = {
+            server: serverInfo.hostnode,
+            gpu_model: serverInfo.specs.gpu.type,
+            gpu_count: 0,
+            ram: 1,
+            vcpus: 2,
+            storage: serverInfo.specs.storage,
+        }
+
+        interaction.reply({ content: '処理中...', ephemeral: true }); // まず応答を返す
+        const stopRes = await tensordock.stop(serverId);
+        if (stopRes.success === false){
+            // サーバーの停止に失敗した場合
+            console.log(`serverの停止に失敗しました。serverId: ${serverId}`);
+            console.log(stopRes.error);
             // 管理者にメッセージを送信
             const user = await interaction.client.users.fetch(myDiscordId);
-            const dashboardUrl = `https://dashboard.tensordock.com/manage/${server_id}/`;
-            await user.send(`serverの停止に失敗しました。\n ${res.error} \n server_id: ${server_id}` + `\n dashboard: ${dashboardUrl}`);
-            await interaction.reply(`stopに失敗しました。${res.error}`);
+            const dashboardUrl = `https://dashboard.tensordock.com/manage/${serverId}/`;
+            await user.send(`serverの停止に失敗しました。\n ${stopRes.error} \n serverId: ${serverId}` + `\n dashboard: ${dashboardUrl}`);
+            await interaction.followUp(`stopに失敗しました。${stopRes.error}`);
             return;
+        } else {
+            // サーバーは停止している
+            // リソースを開放する
+            const modifyRes = await tensordock.modify(stopParam);
+            if (modifyRes.success === true){
+                await interaction.followUp(`リソースを解放しました。\n\`\`\`${serverId}\`\`\``);
+                return;
+            } else {
+                // 開放に失敗した場合
+                // 管理者にメッセージを送信
+                const user = await interaction.client.users.fetch(myDiscordId);
+                const dashboardUrl = `https://dashboard.tensordock.com/manage/${serverId}/`;
+                await user.send(`リソースの解放に失敗しました。\n ${modifyRes.error} \n serverId: ${serverId}` + `\n dashboard: ${dashboardUrl}`);
+                await interaction.followUp(`リソースの解放に失敗しました。${modifyRes.error}`);
+                return;
+            }
         }
-        await interaction.reply(`stopしました。\n\`\`\`${server_id}\`\`\``);
-
     },
 };
+
